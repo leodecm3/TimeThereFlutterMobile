273520dca6a7457d1ec6a7e768141d4f82827bcf
leodecm3 <leodecm3@gmail.com>
2019-07-08 23:14:38 +1000

    2019 07 08 23 14 38

--- lib/main.dart
+++ lib/main.dart
@@ -255,13 +255,13 @@ iconeLm(_addHours, localzacao) {
     if (localzacao == "Florida") {
       // _icone = Container();
       _icone = Image.asset(
-        '../direita.jpg',
+        '../direita-usa.jpg',
         scale: 7,
       );
     } else {
       //sydney
       _icone = Image.asset(
-        '../esquerda.jpg',
+        '../esquerda-aus.jpg',
         scale: 7,
       );
     }
